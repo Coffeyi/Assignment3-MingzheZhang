763df688061d1e7e3a9324e5c4906eecbe348f00
MingZhe Zhang <mingzzhang1@student.unimelb.edu.au>
2022-05-31 15:21:34 +0800

    1
    1

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-function toHome() {
-    location.replace(index.html)
-  }
+function toHome(){
+    window.location.href="index.html";
+}
+
+function toProductlist(){
+    window.location.href="productlist.html"
+}
+
+function toProductPage(){
+    window.location.href="productpage.html"
+}
+
+function toCart(){
+    window.location.href="cart.html"
+}
